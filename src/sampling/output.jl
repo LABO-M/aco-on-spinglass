dac17f3ec648ca84cf31e191962140c9b73e5f94
ShoSampo <shosampo@gmail.com>
2023-12-07 01:46:18 +0900

    単位のフォーマット関数名の変更

--- src/sampling/output.jl
+++ src/sampling/output.jl
@@ -1,7 +1,7 @@
 using CSV, DataFrames
 
 # Convert the numeric value to a string format with K, M, G, etc.
-function format_num(value::Int)
+function int_to_SI_prefix(value::Int)
     if value % 1_000_000_000 == 0
         return string(value ÷ 1_000_000_000) * "G"
     elseif value % 1_000_000 == 0
@@ -15,7 +15,7 @@ end
 
 # Function to save Z values to a CSV file with optional downsampling
 function save_Z_to_csv(Z::Vector{Float64}, filename::String, interval::Int=1000)
-    t_values = collect(1:interval:length(Z))
+    t_values = collect(1:length(Z))
     Z_downsampled = Z[t_values]
     df = DataFrame(t=t_values, Z=Z_downsampled)
     CSV.write(filename, df)
