2e6c88cad9d279890640900c3dda054f1859b6de
Taiyo Shimizu <taiyoshimizu35@gmail.com>
2024-12-11 13:06:43 +0900

    modified file_path

--- src/sampling/simulated_annealing/output.py
+++ src/sampling/simulated_annealing/output.py
@@ -7,15 +7,15 @@ def ensure_dir_exists(file_path):
     if dir_name and not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-def list_to_csv(name, data):
-    file_path = f'/home/mori-lab/shimizu/ACO/ACO-on-SpinGlass/src/sampling/simulated_annealing/data/{name}.csv'
+def list_to_csv(name, data, seed, iter):
+    file_path = f'/home/mori-lab/shimizu/ACO/ACO-on-SpinGlass/src/sampling/simulated_annealing/data/seed{seed}/iter{iter:.0e}/{name}.csv'
     ensure_dir_exists(file_path)  # ディレクトリの存在確認
     with open(file_path, 'w', newline='') as f:
         for _ in data:
             print(_, sep=',', file=f)
 
-def matrix_to_csv(name, data):
-    file_path = f'/home/mori-lab/shimizu/ACO/ACO-on-SpinGlass/src/sampling/simulated_annealing/data/{name}.csv'
+def matrix_to_csv(name, data, seed, iter):
+    file_path = f'/home/mori-lab/shimizu/ACO/ACO-on-SpinGlass/src/sampling/simulated_annealing/data/seed{seed}/iter{iter:.0e}/{name}.csv'
     ensure_dir_exists(file_path)  # ディレクトリの存在確認
     with open(file_path, 'w', newline='') as f:
         writer = csv.writer(f)
