f360ad67b8e25af5601fe8a3742826e36bb4aaa8
Taiyo Shimizu <taiyoshimizu35@gmail.com>
2025-03-18 16:17:57 +0900

    modified file path

--- src/sampling/python_alpha_annealing/output.py
+++ src/sampling/python_alpha_annealing/output.py
@@ -8,14 +8,14 @@ def ensure_dir_exists(file_path):
         os.makedirs(dir_name)
 
 def list_to_csv(name, data, seed, iter):
-    file_path = f'../../../data/spinglass/beta/annealing/seed{seed}/iter{iter:.0e}/{name}.csv'
+    file_path = f'../../../data/ising/beta/annealing/seed{seed}/iter{iter:.0e}/{name}.csv'
     ensure_dir_exists(file_path)  # ディレクトリの存在確認
     with open(file_path, 'w', newline='') as f:
         for _ in data:
             print(_, sep=',', file=f)
 
 def matrix_to_csv(name, data, seed, iter):
-    file_path = f'.../../../data/spinglass/beta/annealing/seed{seed}/iter{iter:.0e}/{name}.csv'
+    file_path = f'../../../data/ising/beta/annealing/seed{seed}/iter{iter:.0e}/{name}.csv'
     ensure_dir_exists(file_path)  # ディレクトリの存在確認
     with open(file_path, 'w', newline='') as f:
         writer = csv.writer(f)
