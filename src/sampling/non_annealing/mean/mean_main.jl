af0847ede90502b4f9636bd548539a0d5e2b12e6
Taiyo Shimizu <taiyoshimizu35@gmail.com>
2025-07-08 18:30:26 +0900

    first commit

---
+++ src/sampling/non_annealing/mean/mean_main.jl
@@ -0,0 +1,73 @@
+using ArgParse
+include("mean_sampling.jl")
+using .simulation
+
+function main(args)
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "--power"
+        help = "the number that power of 2"
+        default = 5
+        arg_type = Int
+
+        "--tau"
+        help = "evaporation hyper parameter"
+        default = 100.0
+        arg_type = Float64
+
+        "--beta"
+        help = "coefficient of external field"
+        default = 0.01
+        arg_type = Float64
+
+        "--start_alpha"
+        help = "alpha value at the start point"
+        default = 0.5
+        arg_type = Float64
+
+        "--start_seed"
+        help = "random seed at the start point"
+        default = 42
+        arg_type = Int
+
+        "--iter"
+        help = "iteration number"
+        default = 100000
+        arg_type = Int
+
+        "--sample"
+        help = "sample size"
+        default = 100
+        arg_type = Int
+
+        "--magnetic"
+        help = "magnetic field value"
+        default = 0.0
+        arg_type = Float64
+
+        "--interaction"
+        help = "coefficient of interaction"
+        default = 0.1
+        arg_type = Float64
+
+    end
+
+    parsed_args = parse_args(args, s)
+    power = parsed_args["power"]
+    tau = parsed_args["tau"]
+    beta = parsed_args["beta"]
+    start_alpha = parsed_args["start_alpha"]
+    start_seed = parsed_args["start_seed"]
+    iter = parsed_args["iter"]
+    sample = parsed_args["sample"]
+    magnetic = parsed_args["magnetic"]
+    interaction = parsed_args["interaction"]
+
+    filename_z_mean, filename_spins_mean = simulation.sampling(power, tau, beta, start_alpha, start_seed, iter, sample, magnetic, interaction)
+    println("Saved $filename_z_mean and $filename_spins_mean")
+
+    
+end
+
+isinteractive() || main(ARGS)
---
+++ src/sampling/non_annealing/var/var_sampling.jl
@@ -0,0 +1,141 @@
+module simulation
+
+using Random
+using CSV
+using DataFrames
+using LinearAlgebra
+using Statistics
+using Base.Threads
+using Printf
+using FilePathsBase
+using Distributed
+using SharedArrays
+
+# --- 共通関数群 ---
+function calculate_energy(spins, h, J)
+    return -sum(h .* spins) - sum(J .* (spins * spins')) / max(length(spins) - 1, 1)
+end
+
+function calculate_external_effect(determined_spins, J, new_spin_index)
+    return -mean(determined_spins .* J[1:length(determined_spins), new_spin_index])
+end
+
+function decision_probabilities(st1, st0, index, alpha, beta, external_effect)
+    z = st1[index] / (st1[index] + st0[index])
+    return (z^alpha * exp(-beta * external_effect)) / (z^alpha * exp(-beta * external_effect) + (1 - z)^alpha * exp(beta * external_effect))
+end
+
+function simulate(power, tau, beta, start_alpha, seed, iter, sample, magnetic, interaction)
+    #seed
+    rng = MersenneTwister(seed)
+
+    # 初期化
+    n = 2 ^ power
+    J = ones(n, n) * interaction
+    J[diagind(J)] .= 0
+    st1 = ones(n) .* exp(n * (magnetic + interaction)) .* (tau / 2)
+    st0 = ones(n) .* exp(n * (magnetic + interaction)) .* (tau / 2)
+    alpha = start_alpha
+    z_mean_series = Float64[]
+    spins_mean_series = Float64[]
+
+    for i in 1:iter
+        determined_spins = [2 * (rand(rng) < 0.5) - 1]
+
+        for new_spins in 2:n
+            external_effect = -magnetic + calculate_external_effect(determined_spins, J, new_spins)
+            prob = decision_probabilities(st1, st0, new_spins, alpha, beta, external_effect)
+            new_spin = 2 * (rand(rng) < prob) - 1
+            push!(determined_spins, new_spin)
+        end
+
+        energy = calculate_energy(determined_spins, h, J)
+        st1 .= st1 .* exp(-1 / tau) .+ exp(-energy) .* ((determined_spins .+ 1) ./ 2)
+        st0 .= st0 .* exp(-1 / tau) .+ exp(-energy) .* ((-determined_spins .+ 1) ./ 2)
+        z = st1 ./ (st1 .+ st0)
+        push!(z_var_series, var(z))
+        push!(spins_var_series, var(determined_spins))
+
+    end
+
+    return z_var_series, spins_var_series
+end
+
+function simulate(power, tau, beta, start_alpha, seed, iter, sample, interaction)
+    #seed
+    rng = MersenneTwister(seed)
+
+    # 初期化
+    n = 2 ^ power
+    J = ones(n, n) * interaction
+    J[diagind(J)] .= 0
+    st1 = ones(n) .* exp(n * interaction) .* (tau / 2)
+    st0 = ones(n) .* exp(n * interaction) .* (tau / 2)
+    alpha = start_alpha
+    z_mean_series = Float64[]
+    spins_mean_series = Float64[]
+
+    for i in 1:iter
+        determined_spins = [2 * (rand(rng) < 0.5) - 1]
+
+        for new_spins in 2:n
+            external_effect = calculate_external_effect(determined_spins, J, new_spins)
+            prob = decision_probabilities(st1, st0, new_spins, alpha, beta, external_effect)
+            new_spin = 2 * (rand(rng) < prob) - 1
+            push!(determined_spins, new_spin)
+        end
+
+        energy = calculate_energy(determined_spins, 0, J)
+        st1 .= st1 .* exp(-1 / tau) .+ exp(-energy) .* ((determined_spins .+ 1) ./ 2)
+        st0 .= st0 .* exp(-1 / tau) .+ exp(-energy) .* ((-determined_spins .+ 1) ./ 2)
+        z = st1 ./ (st1 .+ st0)
+        push!(z_var_series, var(z))
+        push!(spins_var_series, var(determined_spins))
+
+    end
+
+    return z_var_series, spins_var_series
+end
+
+function sampling(power::Int, tau::Float64, beta::Float64, start_alpha::Float64, start_seed::Int, iter::Int, sample::Int, magnetic::Float64, interaction::Float64)
+    z_var_array = SharedArray{Float64}(iter, sample)
+    spins_var_array = SharedArray{Float64}(iter, sample)
+
+    @sync @distributed for i in 1:sample
+        seed = start_seed + (i-1) * 1000
+        if magnetic == 0.0
+            z_var_array[:, i], spins_var_array[:, i] = simulate(power, tau, beta, start_alpha, seed, iter, sample, interaction)
+        else
+            z_var_array[:, i], spins_var_array[:, i] = simulate(power, tau, beta, start_alpha, seed, iter, sample, magnetic, interaction)
+        end
+    end
+
+    # --- DataFrame 化 ---
+    df_z_var = DataFrame(step = 1:iter)
+    for i in 1:sample
+        df_z_var[!, Symbol("sample$i")] = z_var_array[:, i]
+    end
+    df_spin_var = DataFrame(step = 1:iter)
+    for i in 1:sample
+        df_spin_var[!, Symbol("sample$i")] = spins_var_array[:, i]
+    end
+
+    # --- 出力先ディレクトリ作成 ---
+    dir_switch = magnetic == 0.0 ? "symmetric" : "asymmetric"
+    iter_str = @sprintf("%.1e", iter)
+    dir_path = "/home/mori-lab/shimizu/aco/data/ising/nonannealing/seed$(start_seed)/iter$(iter_str)/$(dir_switch)"
+    mkpath(dir_path)
+
+    # --- ファイル名と保存 ---
+    filename_z_var = @sprintf("beta%.1e_sample%.1e_n2^%d_tau%.1e_z_var.csv", beta, sample, power, tau)
+    full_path_z_var = joinpath(dir_path, filename_z_var)
+    CSV.write(full_path_z_var, df_z_var)
+    filename_spins_var = @sprintf("beta%.1e_sample%.1e_n2^%d_tau%.1e_spins_var.csv", beta, sample, power, tau)
+    full_path_spins_var = joinpath(dir_path, filename_spins_var)
+    CSV.write(full_path_spins_var, df_spin_var)
+
+    return filename_z_var, filename_spins_var
+
+end
+
+end
